0114b01ace5f47c7cd8f53862af1f9ec9afeba00
Kelin Field <kfield111@hotmail.com>
2020-08-13 00:53:05 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    if BRANDS.include? "#{brand}" ? nil : BRANDS << brand
+    BRANDS.include? "#{brand}" ? nil : BRANDS << brand
     end
   end
 
